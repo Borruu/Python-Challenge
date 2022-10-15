01b7387dfcaf7550f6ea55ed45c6c9a90db6c7f6
Brianna <oconnor.brianna@gmail.com>
2022-10-15 17:22:00 +0800

    u=increase, decrease and average change

--- PyBank/main.py
+++ PyBank/main.py
@@ -16,7 +16,6 @@ inc_month = "month"
 dcr = 0
 dcr_month = "month"
 
-
 with open("budget_data.csv", "r", encoding="utf") as budget:
     rb = csv.reader(budget, delimiter=",")
     header = next(budget)
@@ -27,9 +26,34 @@ with open("budget_data.csv", "r", encoding="utf") as budget:
         net = (net + current_value)
         change_occurred = (current_value - int(previous_value))
         change.append(change_occurred)
+        if change_occurred > inc:
+            inc = change_occurred
+            inc_month = row[0]
+        if change_occurred < dcr:
+            dcr = change_occurred
+            dcr_month = row[0]
         previous_value = row[1]
-print(f"{count}")
-print(f"{net}")
-print(f"{change}")
-check = len(change)
-print(f"{check}")
+
+change.pop(0)
+
+
+def average(numbers):
+    length = len(numbers)
+    total = 0.0
+    for number in numbers:
+        total += number
+    return total/length
+
+
+avg = average(change)
+avg = round(avg, 2)
+# print(f"{count}")
+# print(f"{net}")
+# print(f"{change}")
+#check = len(change)
+# print(f"{check}")
+print(f"Total months: {count}")
+print(f"Total: ${net}")
+print(f"Average change: ${avg}")
+print(f"Greatest increase in profits: {inc_month} (${inc})")
+print(f"Greatest decrease in profits: {dcr_month} (${dcr})")
