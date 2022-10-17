935da50d264db5d26e7de9297bde2e2ee77c306d
Brianna <oconnor.brianna@gmail.com>
2022-10-17 10:50:51 +0800

    tidy code and comments main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,8 +1,11 @@
 from calendar import month
 import os
 import csv
+
+# Navigate to source csv file
 file = 'Resources/budget_data.csv'
 
+# Variables
 count = 0
 net = 0
 current_value = 0
@@ -15,10 +18,13 @@ inc_month = "month"
 dcr = 0
 dcr_month = "month"
 
+# Read csv file
 with open(file, "r", encoding="utf") as budget:
     rb = csv.reader(budget, delimiter=",")
+    # Store header row
     header = next(budget)
 
+# Loop through rows to count months, get total profit/loss, greatest increase, greatest decrease and create list of change values
     for row in rb:
         count = count + 1
         current_value = int(row[1])
@@ -32,8 +38,10 @@ with open(file, "r", encoding="utf") as budget:
             dcr = change_occurred
             dcr_month = row[0]
         previous_value = row[1]
-
+# Remove first value from change list
 change.pop(0)
+
+# Function to find average change
 
 
 def average(numbers):
@@ -44,18 +52,22 @@ def average(numbers):
     return total/length
 
 
+# Get average change and round to 2 decimal places
 avg = average(change)
 avg = round(avg, 2)
 
+# Print results to Terminal
 print(f"Total months: {count}")
 print(f"Total: ${net}")
 print(f"Average change: ${avg}")
 print(f"Greatest increase in profits: {inc_month} (${inc})")
 print(f"Greatest decrease in profits: {dcr_month} (${dcr})")
 
+# Store results text in list
 lines = [(f"Total months: {count}"), (f"Total: ${net}"), (f"Average change: ${avg}"), (f"Greatest increase in profits: {inc_month} (${inc})"),
          (f"Greatest decrease in profits: {dcr_month} (${dcr})")]
-# os.chdir(r'C:\Users\30798\Desktop\Python\Module_03_Python2\Python-Challenge\PyBank\analysis')
+
+# Navigate to analysis folder and create/overwrite Analysis text file with results list
 file_a = 'analysis/Analysis.txt'
 with open(file_a, 'w') as a:
     for line in lines:
