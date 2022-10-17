b788168fb71c5e7733918e442d85be77291dcf31
Brianna <oconnor.brianna@gmail.com>
2022-10-17 11:11:22 +0800

    tidy and comment main.py code, delet main_local.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,21 +1,30 @@
 import os
 import csv
-# os.chdir(r'C:\Users\30798\Desktop\Python\Module_03_Python2\Python-Challenge\PyPoll\Resources')
+
+# Navigate to source csv file
 path = 'Resources/election_data.csv'
+
+# Variables
 total = 0
 list_cand = []
 tally = []
 per = []
 
+# Read csv file
 with open(path, "r") as file:
     ed = csv.reader(file, delimiter=",")
-    next(ed, None)
+    # store header row
+    header = next(ed, None)
+
+    # loop through rows to get list of candidates and total number of votes
     for row in ed:
         if row[2] not in list_cand:
             list_cand.append(row[2])
         total = int(total) + 1
     for name in list_cand:
         tally.append(0)
+
+# Loop through rows again to add vote count for each candidate to tally list
 with open(path, "r") as file:
     eds = csv.reader(file, delimiter=",")
     next(eds, None)
@@ -24,17 +33,21 @@ with open(path, "r") as file:
             i = list_cand.index(name)
             if row[2] == list_cand[i]:
                 tally[i] += 1
+
+# Loop through tally list to work out percentage of votes for each candidate, round value to 3 decimal places and store results in per list
 for votes in tally:
     ita = tally.index(votes)
     value = tally[ita]/total*100
     value = round(value, 3)
     per.append(value)
 
+# Print results to Terminal for total votes
 print("Election Results")
 print("-------------------------")
 print(f"Total Votes: {total}")
 print("-------------------------")
 
+# Find maximum value in per list to declare winner
 a = 0
 max = 0
 for n in per:
@@ -43,22 +56,26 @@ for n in per:
         max = n
 winner = list_cand[a]
 
+# Store results text to lines list for total votes
 lines = [("Election Results"), ("-------------------------"),
          (f"Total Votes: {total}"), ("-------------------------")]
 
+# Concatenate results for each candidate, print to Terminal and add to lines list
 for name in list_cand:
     j = list_cand.index(name)
     print(f"{list_cand[j]}: {per[j]}% ({tally[j]})")
     lines.append((f"{list_cand[j]}: {per[j]}% ({tally[j]})"))
 
+# Add winner to lines list
 lines.append(("-------------------------"))
 lines.append((f"Winner: {winner}"))
 lines.append(("-------------------------"))
 
+# Print winner to Terminal
 print("-------------------------")
 print(f"Winner: {winner}")
 
-# os.chdir(r'C:\Users\30798\Desktop\Python\Module_03_Python2\Python-Challenge\PyPoll\analysis')
+# Navigate to analysis folder, create/overwrite Analysis text file with results
 path_a = 'analysis/Analysis.txt'
 with open(path_a, 'w') as b:
     for line in lines:
